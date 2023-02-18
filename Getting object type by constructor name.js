3b6f0b397d06f43e9bd3620a677096de9faa245d
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-18 13:56:34 +0530

    first commit

---
+++ Getting object type by constructor name.js
@@ -0,0 +1,2 @@
+console.log =Object.prototype.toString.call("42")
+
---
+++ typeof.js
@@ -0,0 +1,24 @@
+// let user = 'husain saify'
+
+// console.log(typeof user);
+
+// let num = 42
+
+// console.log(typeof num);
+
+// let boolean = 5>6
+
+// console.log(typeof boolean);
+
+// let user = Error()
+
+// console.log(typeof user)
+
+
+let var1;
+
+console.log(var1);
+
+let user
+
+console.log(user);
